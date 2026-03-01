1735e6bdc2e733bb7c872752c369ca070f53c649
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-02-28 23:39:13 -0600

    eleccion de ejercicio

--- Vasquez_Herbert_For_anidado/Program.cs
+++ Vasquez_Herbert_For_anidado/Program.cs
@@ -1,1 +1,29 @@
 ﻿//creacion de la tarea
+//la seleccion de los ejercicios xd
+Console.WriteLine("Mostrar un cuadrado de 5x5 con números 1.\r\n\r\nMostrar un rectángulo de 4 filas y 6 columnas con asteriscos.\r\n\r\nMostrar un triángulo creciente de 5 filas con asteriscos.\r\n\r\nMostrar un triángulo invertido de 5 filas.\r\n\r\nMostrar una tabla de multiplicar del 1 al 5 (todas juntas).\r\n\r\nMostrar una matriz 3x3 con números consecutivos del 1 al 9.\r\n\r\nMostrar las combinaciones posibles de números del 1 al 3 (ejemplo: 1-1, 1-2...).\r\n\r\nMostrar una tabla donde las filas sean del 1 al 4 y las columnas del 1 al 4.\r\n\r\nMostrar un patrón donde cada fila tenga el número de la fila repetido.\r\n\r\nMostrar un patrón numérico como:");
+int opcion = int.Parse(Console.ReadLine());
+switch (opcion)
+{
+    case 1:
+        break;
+        case 2:
+        break;
+            case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        case 6:
+        break;
+        case 7:
+        break;
+        case 8:
+        break;
+        case 9:
+        break;
+        case 10:
+        break;
+        default
+        break;
+}
